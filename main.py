dbf698910eff231d6a4207840775b5d86838b3b2
Celso Netto <celso.bmnetto1@gmail.com>
2025-05-14 23:15:22 -0300

    update main

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
 import mysql.connector
 
 def main():
-    
-    # inicia bd com essas informacoes
     conn = mysql.connector.connect(
         host='localhost',
         user='root',
-        password="password", # CHANGE BEFORE COMMITING
+        password="101718", # CHANGE BEFORE COMMITING
         port=3306
     )
 
     cursor = conn.cursor()
 
-    options(cursor)
+    while True:
+        print("\nOpções:")
+        print("1 - Criar banco de dados")
+        print("2 - Cadastrar produto")
+        print("3 - Cadastrar cliente")
+        print("4 - Deletar banco de dados")
+        print("0 - Sair")
+
+        option = input("Escolha uma opção: ")
+
+        if option == "1":
+            create_db(cursor, 'ecommerce.sql')
+        elif option == "2":
+            create_product(cursor)
+        elif option == "3":
+            create_client(cursor)
+        elif option == "4":
+            drop_db(cursor)
+        elif option == "0":
+            print("Fim")
+            break
+        else:
+            print("Selecione uma opção válida.")
 
     conn.commit()
     cursor.close()
     conn.close()
 
-
-def options(cursor):
-    print("Opções:")
-    print("1 - Criar banco de dados")
-    print("2 - Cadastrar produto")
-    print("3 - Cadastrar cliente")
-    print("4 - Deletar banco de dados")
-
-    option = (input("Escolha uma opcao: "))
-
-    if option == "1":
-        create_db(cursor, 'ecommerce.sql')
-    elif option == "2":
-        create_product(cursor)
-    elif option == "3":
-        create_client(cursor)
-    elif option == "4":
-        drop_db(cursor)
-    else:
-        print("Selecione uma opcao valida.")
-
-
 def create_db(cursor, filename):
     try:
-        with open(filename, "r", "utf-8") as file:
+        with open(filename, "r", encoding="utf-8") as file:
             sql_script = file.read()
 
         for statement in sql_script.strip().split(";"):
@@ -57,6 +55,12 @@ def drop_db(cursor):
     try:
         cursor.execute("DROP DATABASE ecommerce")
         print("Banco de dados deletado com sucesso.")
+        opcao = input("Deseja apagar usuários também? S/N: " ).capitalize()
+        if opcao == "S":
+            cursor.execute("DROP USER IF EXISTS 'administrador'@'localhost';")
+            cursor.execute("DROP USER IF EXISTS 'gerente'@'localhost';")
+            cursor.execute("DROP USER IF EXISTS 'funcionario'@'localhost';")
+            print("Usuarios deletados")
     except Exception as e:
         print(f"Erro ao deletar banco: {e}")
 
@@ -67,6 +71,7 @@ def create_product(cursor):
     valor = input('Valor: ')
 
     try:
+        cursor.execute("USE ecommerce")
         cursor.execute(
             "INSERT INTO produto (nome, quantidade, descricao, valor) VALUES (%s, %s, %s, %s)",
             (nome, quantidade, descricao, valor)
@@ -82,6 +87,7 @@ def create_client(cursor):
     nascimento = input('Data de nascimento (YYYY-MM-DD): ')
 
     try:
+        cursor.execute("USE ecommerce")
         cursor.execute(
             "INSERT INTO cliente (nome, sexo, idade, nascimento) VALUES (%s, %s, %s, %s)",
             (nome, sexo, idade, nascimento)
